97cbb57fe73f615217dbbd4d6b52cbc0e226ae27
Auvar Mahsa Fahlevi <92295750+Auvarmf@users.noreply.github.com>
2022-06-13 19:59:06 +0700

    Update Subsidi.cpp

--- Subsidi.cpp
+++ Subsidi.cpp
@@ -1,41 +1,44 @@
 #include <iostream>
 #include <queue.h>
 using namespace std;
-int main(){
-	queue <int> Q;
-	int q, i;
+
+	void Subsidi(){
+		queue <int> Q;
+		int q, i;
 	
-	while(true){
-		cout<<"<<========================================>>"<<endl;
-		cout<<"||    1. Jumlah antrian sembako           ||"<<endl;
-		cout<<"||    2. Masukkan data warga ke antrian   ||"<<endl;
-		cout<<"||    3. Hapus data warga dari antrian    ||"<<endl;
-		cout<<"||    4. Exit                             ||"<<endl;
-		cout<<"<<========================================>>"<<endl;
-		cout<<endl;
-		cout<<"Masukkan pilihan anda : ";cin>>q;
-		system("cls");
-		
-		switch(q){
-			case 1 : 
-				cout<<"Jumlah antrian : "<< Q.size() <<endl;
-				break;
+		while(1){
+			cout<<"<<========================================>>"<<endl;
+			cout<<"||    1. Jumlah antrian sembako           ||"<<endl;
+			cout<<"||    2. Masukkan data warga ke antrian   ||"<<endl;
+			cout<<"||    3. Hapus data warga dari antrian    ||"<<endl;
+			cout<<"||    4. Exit                             ||"<<endl;
+			cout<<"<<========================================>>"<<endl;
+			cout<<endl;
+			cout<<"Masukkan pilihan anda : ";cin>>q;
+			system("cls");
 			
-			case 2 : 
-				cout<<"Masukkan data  : "; cin>>i; Q.push(i);
+			switch(q){
+				case 1 : cout<<"Jumlah antrian : "<<Q.size()<<endl;
 				break;
-			
-			case 3 : 
-				i = Q.front();
-				Q.pop();
-				cout<<"Sembako "<< i << " sudah diambil" << endl;
+				
+				case 2 : cout<<"Masukkan data  : ";cin>>i; Q.push(i);
 				break;
-			
-			case 4 : 
-				exit(1);
+				
+				case 3 : i = Q.front();
+						 Q.pop();
+						 cout<<"Data "<<i<<" sudah dihapus"<<endl;
 				break;
-			default : 
-				cout<<"Pilihan anda salah"<<endl;
+				
+				case 4 : exit(1);
+				break;
+				
+				default : cout<<"Pilihan anda salah"<<endl;
+			}
 		}
 	}
+
+int main(){
+	
+	Subsidi();
+	
 }
